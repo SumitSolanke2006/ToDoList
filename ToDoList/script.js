1abbbf54dc0bbebfc7e8950d67f9e1b05a557517
Sumit Solanke <sumitsolanke2006@gmail.com>
2025-09-17 18:04:45 +0530

    Update script.js

--- ToDoList/script.js
+++ ToDoList/script.js
@@ -1,23 +1,19 @@
-const inputBox = document.getElementById("input-box");
-const listContainer = document.getElementById("list-container");
-
+const inputBox = document.getElementById("input box");
+const listContainer = document.getElementById("list container");
 function addTask() {
-    if (inputBox.value === '') {
+    if (inputBox.value=== '') {
         alert("You must write something!");
     } else {
         let li = document.createElement("li");
         li.innerHTML = inputBox.value;
         listContainer.appendChild(li);
-
         let span = document.createElement("span");
         span.innerHTML = "\u00d7";
         li.appendChild(span);
-
         saveData();
     }
     inputBox.value = "";
 }
-
 listContainer.addEventListener("click", function (e) {
     if (e.target.tagName === "LI") {
         e.target.classList.toggle("checked");
@@ -27,20 +23,16 @@ listContainer.addEventListener("click", function (e) {
         saveData();
     }
 }, false);
-
-// Add task by pressing Enter
 inputBox.addEventListener("keypress", function (e) {
     if (e.key === "Enter") {
         addTask();
     }
 });
-
 function saveData() {
     localStorage.setItem("data", listContainer.innerHTML);
 }
-
 function showTask() {
     listContainer.innerHTML = localStorage.getItem("data");
 }
-
 showTask();
+
